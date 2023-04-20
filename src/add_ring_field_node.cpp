852cedd563054b9198b92934b15f34944a6ae7be
KANBE8810 <s19c1034bq@s.chibakoudai.jp>
2023-04-20 17:50:37 +0900

    ros2に対応

--- src/add_ring_field_node.cpp
+++ src/add_ring_field_node.cpp
@@ -1,9 +1,13 @@
-#include <pcl/point_types.h>
-#include <ros/ros.h>
-#include <sensor_msgs/PointCloud2.h>
-#include <pcl_conversions/pcl_conversions.h>
+#include <chrono>
+#include <iostream>
+#include <memory>
 
-static ros::Publisher  s_pub;
+#include "pcl/point_types.h"
+#include "pcl_conversions/pcl_conversions.h"
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
+
+using namespace std::chrono_literals;
 
 struct PointXYZRI
 {
@@ -13,43 +17,41 @@ struct PointXYZRI
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 } EIGEN_ALIGN16;
 
-POINT_CLOUD_REGISTER_POINT_STRUCT (PointXYZRI,
-                                   (float, x, x)
-                                   (float, y, y)
-                                   (float, z, z)
-                                   (float, intensity, intensity)
-                                   (uint16_t, ring, ring))
+POINT_CLOUD_REGISTER_POINT_STRUCT(PointXYZRI,
+                                  (float, x, x)
+                                  (float, y, y)
+                                  (float, z, z)
+                                  (float, intensity, intensity)
+                                  (uint16_t, ring, ring))
 
-void callback(const sensor_msgs::PointCloud2ConstPtr& msg)
+void callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
 {
   pcl::PointCloud<PointXYZRI>::Ptr cloud_xyzri(new pcl::PointCloud<PointXYZRI>);
   pcl::fromROSMsg(*msg, *cloud_xyzri);
 
   // Add extra field "ring" to PointCloud2
-  sensor_msgs::PointCloud2 cloud_ros;
-  for(int i=0; i< cloud_xyzri->points.size(); ++i){
-      cloud_xyzri->points[i].ring = 0;
+  for (int i = 0; i < cloud_xyzri->points.size(); ++i) {
+    cloud_xyzri->points[i].ring = 0;
   }
   std::cout << "pcd " << cloud_xyzri->width << " " << cloud_xyzri->height << std::endl;
   cloud_xyzri->is_dense = true;
+  sensor_msgs::msg::PointCloud2 cloud_ros;
   pcl::toROSMsg(*cloud_xyzri, cloud_ros);
 
   cloud_ros.header = msg->header;
   // Do something with cloud_ros here
 
-  s_pub.publish(cloud_ros);
+  static rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr s_pub;
+  s_pub->publish(cloud_ros);
 }
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-  ros::init(argc, argv, "add_ring_to_pointcloud2");
-  ros::NodeHandle nh;
-
-  ros::Subscriber sub = nh.subscribe("input_pointcloud", 10, callback);
-  s_pub = nh.advertise<sensor_msgs::PointCloud2>("points_raw", 10);
-
-  ros::spin();
-
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<rclcpp::Node>("add_ring_to_pointcloud2");
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr sub = node->create_subscription<sensor_msgs::msg::PointCloud2>("input_pointcloud", 10, callback);
+  rclcpp::spin(node);
+  rclcpp::shutdown();
   return 0;
 }
 
